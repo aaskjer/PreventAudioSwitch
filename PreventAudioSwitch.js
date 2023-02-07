e87a61a316f7235caef9e108e0928753665bc205
aaskjer <56720094+aaskjer@users.noreply.github.com>
2023-02-07 17:33:55 +0100

    Update PreventAudioSwitch.js

--- PreventAudioSwitch.js
+++ PreventAudioSwitch.js
@@ -35,13 +35,42 @@ const config = {
     defaultConfig: []
 };
 
-const { WebpackModules, Patcher } = require("powercord/webpack");
+module.exports = !global.ZeresPluginLibrary ? class {
+    constructor() {
+        this._config = config;
+    }
+
+    load() {
+        BdApi.showConfirmationModal("Library plugin is needed",
+            `The library plugin needed for AQWERT'sPluginBuilder is missing. Please click Download Now to install it.`, {
+            confirmText: "Download",
+            cancelText: "Cancel",
+            onConfirm: () => {
+                request.get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", (error, response, body) => {
+                    if (error)
+                        return electron.shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+
+                    fs.writeFileSync(path.join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body);
+                });
+            }
+        });
+    }
+
+    start() { }
+
+    stop() { }
+} : (([Plugin, Library]) => {
+const { WebpackModules } = require("ZeresPluginLibrary");
 const AudioDevice = WebpackModules.getByProps("getAudioSources");
 const AudioSourceChooser = WebpackModules.getByProps("chooseAudioSource");
 
-Patcher.after(AudioDevice, "chooseAudioSource", (_, source) => {
+AudioDevice.chooseAudioSource = function(source) {
   const currentSource = AudioSourceChooser.getCurrentAudioSource();
   if (currentSource && currentSource.id !== source.id) {
     return false;
   }
-});
+  return AudioDevice.__original.chooseAudioSource(source);
+};
+
+    return plugin;
+})(global.ZeresPluginLibrary.buildPlugin(config));
